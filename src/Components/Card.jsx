ff540a64c46b11d08cf353937cd611b295c9b199
Orton Bright <ortonb110@gmail.com>
2023-10-16 22:32:18 +0000

    Minor changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,30 @@
-const Card = (items) => {
-  console.log(items);
+/* eslint-disable react/prop-types */
+const Card = ({name, population, flags, region, capital}) => {
+  const {svg} = flags
 
   return (
-    <div className=" w-[26.4rem] h-[33.6rem] dark:bg-darkElement bg-[#FFFF] rounded-[5px] shadow-md shadow-gray-300 dark:shadow-none cursor-pointer overflow-hidden ">
+    <div className="mx-auto w-[26.4rem] h-[33.6rem] dark:bg-darkElement bg-[#FFFF] rounded-[5px] shadow-md shadow-gray-300 dark:shadow-none cursor-pointer overflow-hidden ">
       <div className="h-[16rem]">
-        <img src={items.items.flags.svg} alt={items.items.nativeName} className="h-full w-full  object-cover"/>
+        <img src={svg} alt={name} className="h-full w-full  object-cover"/>
       </div>
       <div className="p-[2.4rem]">
         <h2 className="font-[800] text-[1.8rem] mb-[1.6rem]">
-          {items.items.name}
+          {name}
         </h2>
         <div className="mb-[4.6rem] text-[1.4rem] capitalize space-y-[4px]">
           <p>
             <span className="font-[700] mr-[3px]">population:</span>
             <span>
-              {new Intl.NumberFormat().format(items.items.population)}
+              {new Intl.NumberFormat().format(population)}
             </span>
           </p>
           <p>
             <span className="font-[700] mr-[3px]">region:</span>
-            <span>{items.items.region}</span>
+            <span>{region}</span>
           </p>
           <p>
             <span className="font-[700] mr-[3px]">capital:</span>
-            <span>{items.items.capital}</span>
+            <span>{capital}</span>
           </p>
         </div>
       </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,9 +6,9 @@ import Card from "../Components/Card";
 const Home = () => {
   const { isLoading, getCountriesData, countries } = useAppContext();
 
-    //console.log(countries[0].name);
+  //console.log(countries[0].name);
   useEffect(() => {
-    getCountriesData()
+    getCountriesData();
   }, []);
 
   if (isLoading) {
@@ -21,11 +21,9 @@ const Home = () => {
 
   return (
     <section className=" h-[calc(100vh-82px)] bg-[#FAFAFA] dark:bg-darkMode dark:text-darkMode  text-lightMode overflow-auto py-[4.8rem] px-[8.0rem] grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[7.5rem] gap-y-[7.5rem]">
-        {
-            countries.map((country, index)=> {
-                return <Card key={index} items={country}/>
-            })
-        }
+      {countries.map((country, index) => {
+        return <Card key={index} {...country} />;
+      })}
     </section>
   );
 };
