55a4e7724e9d97b3cfa215009645d283a4ad7f1f
Orton Bright <ortonb110@gmail.com>
2023-10-18 21:21:14 +0000

    Added a loading skeleton component

---
+++ src/Components/CardSkeleton.jsx
@@ -0,0 +1,17 @@
+const CardSkeleton = () => {
+  return (
+    <div className="w-[26.4rem] h-[33.6rem] dark:bg-darkElement bg-[#FFFF] rounded-[5px] shadow-md shadow-gray-300 dark:shadow-none overflow-hidden">
+      <div className="h-[16rem] bg-gray-300 dark:bg-[#4d6275] animate-pulse"></div>
+      <div className="p-[2.4rem]">
+        <h2 className="font-[800] text-[1.8rem] mb-[1.6rem]"></h2>
+        <div className="mb-[4.6rem] text-[1.4rem] capitalize space-y-[4px]">
+          <p className="w-[14rem] h-[2.6rem] mb-[1rem] bg-gray-300 dark:bg-[#4d6275] animate-pulse"></p>
+          <p className="w-[14rem] h-[6rem] bg-gray-300 dark:bg-[#4d6275] animate-pulse"></p>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardSkeleton;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Loader from "../Components/Loader";
 import { useAppContext } from "../Contexts/AppContext";
 import { useEffect } from "react";
 import Card from "../Components/Card";
-
+import CardSkeleton from "../Components/CardSkeleton";
 const Home = () => {
   const { isLoading, getCountriesData, countries } = useAppContext();
 
@@ -13,8 +13,13 @@ const Home = () => {
 
   if (isLoading) {
     return (
-      <div className="h-[calc(100vh-82px)] grid place-content-center bg-[#FAFAFA] dark:bg-darkMode">
-        <Loader />
+      <div className="m-auto h-[calc(100vh-82px)] bg-[#FAFAFA] dark:bg-darkMode dark:text-darkMode  text-lightMode overflow-auto py-[4.8rem] px-[8.0rem] grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-[7.5rem] gap-y-[7.5rem]">
+        {/* <Loader /> */}
+        {/* <CardSkeleton/> */}
+
+        {[0,1,2,3,4,5,6,7,8,9,10].map((item, index) => {
+            return <CardSkeleton key={index}/>
+        })}
       </div>
     );
   }
